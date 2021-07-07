0fdf856e1c2be357b33757c0258aeb972416c25e
Jamie McClelland <jm@mayfirst.org>
2021-07-07 15:23:05 -0400

    let petitionemail hide it's fields
    See: https://github.com/josephlacey/com.aghstrategies.petitionemail/pull/1

--- js/petitionemailcontactref.js
+++ js/petitionemailcontactref.js
@@ -4,17 +4,17 @@
     "return": ["id"],
     "name": "Email_Recipient_System"
   }).done(function(recipientSystemField) {
-    //Page load
-    recipient_system_contactref(recipientSystemField.id);
-    //When the salutation type changes
+    // Set based on initial value.
+    update_contactref_recipient_display(recipientSystemField.id);
+    // Update when the value changes.
     CRM.$("select[id*='custom_" + recipientSystemField.id + "']").change(function() {
-      recipient_system_contactref(recipientSystemField.id);
+      update_contactref_recipient_display(recipientSystemField.id);
     });
   });
 })(CRM.$);
 
-function recipient_system_contactref (fieldId){
-  //Process the selected option
+function update_contactref_recipient_display (fieldId){
+  // Update display based on selected value.
   CRM.api3('CustomField', 'getsingle', {
     "return": ["id"],
     "name": 'Recipient_Contact_Reference'
@@ -22,30 +22,10 @@ function recipient_system_contactref (fieldId){
     //Show Contact Reference Field field
     if (CRM.$("select[id*='custom_" + fieldId + "'] option:selected").val() == 'Contactref') {
       CRM.$("tr[class*='custom_" + recipientContactRefField.id + "']").show();
-      recipient_single_hide_contactref(true);
     }
-    if (CRM.$("select[id*='custom_" + fieldId + "'] option:selected").val() != 'Contactref') {
+    else {
       CRM.$("tr[class*='custom_" + recipientContactRefField.id + "']").hide();
-    }
-    if (CRM.$("select[id*='custom_" + fieldId + "'] option:selected").val() == 'Single') {
-      recipient_single_hide_contactref(false);
     }
   });
 }
 
-function recipient_single_hide_contactref(hide){
-  CRM.api3('CustomField', 'get', {
-    "return": ["id"],
-    "name": {"IN":["Recipient_Name","Recipient_Email"]}
-  }).done(function(recipientSingle) {
-    if (hide) {
-      CRM.$.each(recipientSingle.values, function(id,fieldId) {
-        CRM.$("tr[class*='custom_" + id + "']").hide();
-      });
-    } else {
-      CRM.$.each(recipientSingle.values, function(id, fieldId) {
-        CRM.$("tr[class*='custom_" + id + "']").show();
-      });
-    }
-  });
-}
